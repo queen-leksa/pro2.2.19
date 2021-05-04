092be80169ed4e8c4eaa7b9a296634baf163c72d
Leksa <surnacheva@ithub.ru>
2021-05-04 10:25:47 +0300

    create simple app

---
+++ users/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <div className="app">
+      <h1>Hello App!</h1>
+    </div>
+  }
+}
+
+export default App;
